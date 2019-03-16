08510f14c716af370122b7a3941aacbec14968c1
alena <alena.15243021@gmail.com>
2019-03-16 19:00:40 +0300

    Added animation effects with upgraded version of owl carousel

---
+++ carousel2.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    let owl = $('header .owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        animateOut: 'bounceOutDown',
+        animateIn: 'flipInX',
+        smartSpeed: 1000,
+        dots: true,
+        rewind: true,
+        nav: false
+    });
+    let owl1 = $('main .owl-carousel');
+    owl1.owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        animateOut: 'flipOutY',
+        animateIn: 'flipInX',
+        smartSpeed: 1000,
+        dots: false,
+        nav: true,
+        loop: true
+    });
+});
